875037489f724e15b1a2e1e9ed8222e73a582b22
Hemanth Acharya <acharyahemanth@gmail.com>
2016-08-30 17:00:39 +0530

    added drawFrame() logic to draw points

--- app/src/main/jni/jni_calls/jni_apis.cc
+++ app/src/main/jni/jni_calls/jni_apis.cc
@@ -27,8 +27,9 @@ ShaderReader *shader_reader;
 KLT_gpu *klt;
 cv::Mat cam_image_for_back, cam_image_for_algo;
 std::mutex mtx_camera;
-bool new_cam_image_available=false;
-cv::Mat back_image, algo_image;
+bool new_cam_image_available=false, is_first_frame = true;
+cv::Mat back_image, algo_image, prev_image;
+std::vector<cv::Point2f>prev_corners;
 
 #ifdef __cplusplus
 extern "C" {
@@ -144,6 +145,7 @@ JNIEXPORT void JNICALL Java_hemanth_kltgpgpuandroid_JNICaller_loadResourcesNativ
     klt = new KLT_gpu(num_pyramid_levels,search_window_size,width, height);
 
     mtx_camera.unlock();
+    is_first_frame = true;
 }
 
 JNIEXPORT void JNICALL Java_hemanth_kltgpgpuandroid_JNICaller_processFrameNative
@@ -177,18 +179,39 @@ JNIEXPORT void JNICALL Java_hemanth_kltgpgpuandroid_JNICaller_drawFrameNative
     if(klt==NULL)
         return;
 
-    bool draw_frame=false;
+    bool run_algo=false;
     mtx_camera.lock();
         if(new_cam_image_available){
             back_image = cam_image_for_back.clone();
             new_cam_image_available = false;
-            draw_frame = true;
+            run_algo = true;
         }
     mtx_camera.unlock();
-    if(draw_frame){
+    if(run_algo){
         cv::cvtColor(back_image, algo_image, CV_RGB2GRAY);
-        cv::flip(back_image, back_image, 0);
-        klt->drawFrame(back_image, 1280, 720);
+        if(is_first_frame){
+            prev_corners.clear();
+            cv::goodFeaturesToTrack(algo_image, prev_corners, 100, 0.01, 10);
+            myLOGD("First frame : Found %d corners!",prev_corners.size());
+            prev_image = algo_image.clone();
+            is_first_frame = false;
+        }
+        else {
+            std::vector<bool>error;
+            std::vector<cv::Point2f>tracked_corners;
+            klt->execute(prev_image, algo_image, prev_corners, tracked_corners, error);
+            klt->drawFrame(back_image, 1280, 720, tracked_corners, error);
+            prev_image = algo_image.clone();
+            prev_corners.clear();
+            for(int i=0;i<tracked_corners.size();i++){
+                if(!error[i]){
+                    prev_corners.push_back(tracked_corners[i]);
+                }
+            }
+            tracked_corners.clear();
+            error.clear();
+            myLOGD("KLT :: Tracked %d corners...",prev_corners.size());
+        }
     }
 }
 
